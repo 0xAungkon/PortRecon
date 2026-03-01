4f40a075cad8404d04343289ccae2e9d73ed97dd
0xAungkon <aungkonmalakar@gmail.com>
2026-03-01 23:55:49 +0600

    Add 0.client.js to download IP ranges as a JSON file

---
+++ 0.client.js
@@ -0,0 +1,24 @@
+// Go to https://lite.ip2location.com/nigeria-ip-address-ranges
+// Open the browser console and paste this code to download the IP ranges as a JSON file.
+
+document.querySelectorAll(".text-end").forEach(e => e.remove());
+
+const IPS = [];
+
+document.querySelectorAll("tr").forEach(tr => {
+  const td = tr.querySelectorAll("td");
+  if (td.length >= 2) {
+    IPS.push({
+      start: td[0].textContent.trim(),
+      end: td[1].textContent.trim()
+    });
+  }
+});
+
+const blob = new Blob([JSON.stringify(IPS, null, 2)], { type: "application/json" });
+const url = URL.createObjectURL(blob);
+const a = document.createElement("a");
+a.href = url;
+a.download = "ips.json";
+a.click();
+URL.revokeObjectURL(url);
